e2fbefedcee4007e9b9a321163724e7b6ea94405
Marslino Ashraf Shnouda Sedra <121635291+Marslino882002@users.noreply.github.com>
2024-10-08 02:17:29 +0300

    Add files via upload

---
+++ Src/MentalHealthcare.API/Controllers/AdminController.cs
@@ -0,0 +1,33 @@
+﻿using MentalHealthcare.Domain.Entities;
+using MentalHealthcare.Infrastructure.Persistence;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MentalHealthcare.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AdminController : ControllerBase
+    {
+        private readonly MentalHealthDbContext _dbContext;
+
+        public AdminController(MentalHealthDbContext DbContext)
+        {
+            _dbContext = DbContext;
+        }
+        [HttpPost]
+        [Route("")]
+        public async Task<ActionResult<int>> AddArticle (Article article)
+        {
+
+         article.ArticleId = 0;
+        _dbContext.Set<Article>().Add(article);
+           await _dbContext.SaveChangesAsync();    
+              
+        return Ok(article.ArticleId);
+        }
+
+
+
+    }
+}
