a2717b5572619f5c88d44c532f657e7bb0bc1df5
Marslino Ashraf Shnouda Sedra <121635291+Marslino882002@users.noreply.github.com>
2024-10-17 10:21:39 +0300

    Add files via upload

---
+++ Src/MentalHealthcare.API/MappingProfiles.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using MentalHealthcare.Application;
+using MentalHealthcare.Domain.Dtos;
+using MentalHealthcare.Domain.Entities;
+
+namespace MentalHealthcare.API.Helpers
+{
+    public class MappingProfiles : Profile
+    {
+        public MappingProfiles()
+        {
+            CreateMap<Article, ArticlesDto>()
+              .ForMember(A => A.UploadedBy, O => O.MapFrom(S => S.UploadedBy));
+            CreateMap<Article, ArticlesDto>()
+                                    .ForMember(A => A.AuthorinDto, O => O.MapFrom(S => S.Author));
+
+
+
+
+            CreateMap<Meditation, MeditationDto>()
+             .ForMember(A => A.UploadedByInDto, O => O.MapFrom(S => S.UploadedBy));
+           
+        }
+    }
+}
