da6418b0c8c0f02c0d2405bb076188917d3bd0ce
Marslino Ashraf Shnouda Sedra <121635291+Marslino882002@users.noreply.github.com>
2024-10-17 10:20:20 +0300

    Add files via upload

---
+++ Src/MentalHealthcare.Application/Delete_Meditation_Command.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+using MentalHealthcare.Application.Common;
+using MentalHealthcare.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MentalHealthcare.Application.Meditations.Commands.Delete__Meditation
+{
+    public class Delete_Meditation_Command : IRequest<OperationResult<string>>
+    {
+
+        public Article articleDeleted { get; set; }
+        public int AId { get; set; }
+    }
+}
---
+++ Src/MentalHealthcare.Application/GetByID_Query.cs
@@ -0,0 +1,27 @@
+﻿using MediatR;
+using MentalHealthcare.Application.Common;
+using MentalHealthcare.Domain.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MentalHealthcare.Application.Meditations.Queries.GetByIdM
+{
+    public class GetByID_Query : IRequest<OperationResult<MeditationDto>>
+    {
+
+
+        public int MedtationId { get; set; }
+
+        public GetByID_Query(int id)
+        {
+            id = MedtationId;
+        }
+
+
+
+
+    }
+}
